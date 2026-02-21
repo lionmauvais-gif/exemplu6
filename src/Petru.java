c12a5e2aa5dc4a40fec2cae94455662d38655655
Cirlig Petru <cirligpetru96@gmail.com>
2026-02-21 12:27:03 +0100

    Add to name

--- src/Petru.java
+++ src/Petru.java
@@ -2,6 +2,10 @@ public class Petru {
     static void main() {
 
         System.out.println(String.format("Hello and welcome!"));
+        System.out.println(String.format("Hello FRANCE"));
+        System.out.println(String.format("Hello ESPAGNE"));
+        System.out.println(String.format("Hello MOLDOVA"));
+
 
     }
 }
